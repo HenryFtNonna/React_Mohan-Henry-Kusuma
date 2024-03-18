c5eb00982fc0414cffda01ae04bddea6339770f6
HenryFtNonna <118751951+HenryFtNonna@users.noreply.github.com>
2024-03-18 15:47:34 +0700

    praktik dikelas

--- 09_Introduction React/instalasi/vite-project/src/App.jsx
+++ 09_Introduction React/instalasi/vite-project/src/App.jsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from "./components/NavBar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    Introduction React 
-    <div>halaman div</div> 
-    <h1>Tag H1</h1>
-    <h1>Mohan Henry Kusuma</h1>
-    </>
-  )
+function HeroSection(){
+  return <div>HeroSection</div>;
 }
 
-export default App
+export default function App() {
+  // let user = "henri";
+  let warna = "hitam";
+  console.log("warna = " + warna);
+  let bahasa = "indonesia";
+
+  function tampil (){
+    console.log("halo");
+    warna = "putih";
+    console.log("warna = " + warna);
+  }
+
+  const namaAnak = ["henri","raisal","thalita"];
+  return(
+    <div>
+      <h1>Halaman App</h1>
+      <HeroSection/>
+      <NavBar nama={"john"}/>
+
+
+      <button onClick={tampil}>
+        {bahasa == "inggris" ? "tombol" : "button"}
+      </button>
+      {namaAnak.map((nama) =>(
+      <p>{nama}</p>
+      ))}
+    </div>
+  );
+    }
---
+++ 09_Introduction React/instalasi/vite-project/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function NavBar(props){
+
+
+return (
+    <div>
+    props dari App.js = {props.nama}
+    {props.nama === "john" && <h1>Hai Kamu Admin</h1>}
+    <div>anak dari parent utama</div>
+
+    </div>
+);
+
+}
