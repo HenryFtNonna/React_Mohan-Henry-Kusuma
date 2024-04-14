0fa36debe87daf5a30e57c10aefdc2bbee751fc2
HenryFtNonna <118751951+HenryFtNonna@users.noreply.github.com>
2024-04-14 20:55:55 +0700

    pengumpulan tugas

---
+++ 14_React Hook/Praktikum/vite-project/src/components/header.jsx
@@ -0,0 +1,45 @@
+// header.jsx
+import React, { useState, useEffect } from 'react';
+
+const Navbar = () => {
+    const [activeTab, setActiveTab] = useState("Home");
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+    };
+
+    useEffect(() => {
+        alert("Welcome");
+    }, []); 
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <ul className="nav nav-pills mr-auto">
+                        <b>Simple header</b>
+                    </ul>
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <a className={`nav-link ${activeTab === "Home" ? "active" : ""}`} onClick={() => handleTabClick("Home")} href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={`nav-link ${activeTab === "Features" ? "active" : ""}`} onClick={() => handleTabClick("Features")} href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={`nav-link ${activeTab === "Pricing" ? "active" : ""}`} onClick={() => handleTabClick("Pricing")} href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={`nav-link ${activeTab === "FAQs" ? "active" : ""}`} onClick={() => handleTabClick("FAQs")} href="#">FAQs</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className={`nav-link ${activeTab === "About" ? "active" : ""}`} onClick={() => handleTabClick("About")} href="#">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
